6d666f167f713920cc0ba74fd9727c81e0c9b74c
Areyouokay0 <natsudragneelfiredragongulid@gmail.com>
2023-07-16 22:18:03 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,12 +6,12 @@ class config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5951942864:AAHHsn9GLXHw1LoLHaHr98fG_AklMKgpKU8")
 
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", "8206404"))
-    API_HASH = os.environ.get("API_HASH", "e935d9b56e3fd2c05c743093efb761c9")
+    APP_ID = int(os.environ.get("APP_ID", "820604"))
+    API_HASH = os.environ.get("API_HASH", "e935d9b56e3fd2c5c743093efb761c9")
     # Get these values from my.telegram.org
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "858588280").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "85888280").split())
 
     # Ban Unwanted Members..
     BANNED_USERS = []
@@ -26,5 +26,5 @@ class config(object):
     CHUNK_SIZE = 128
 
     # Database url
-    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Aman:<Aman>@cluster0.zugrjwj.mongodb.net/?retryWrites=true&w=majority")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+sr://n:<n>@cluster0.zugrjwj.mongodb.net/?retryWrites=true&w=majority")
 
